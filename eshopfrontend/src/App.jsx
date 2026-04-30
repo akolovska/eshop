26ba8924b48bd167029f0bc0b8d7b867f202deac
akolovska <ana.kolovska@students.finki.ukim.mk>
2026-04-30 17:50:22 +0200

    fully remove submodules

---
+++ eshopfrontend/src/App.jsx
@@ -0,0 +1,15 @@
+import ProductsPage from './ui/pages/ProductsPage/ProductsPage.tsx'
+import './App.css'
+import { Container } from '@mui/material'
+
+function App() {
+  return (
+    <>
+      <Container>
+        <ProductsPage/>
+      </Container>
+    </>
+  )
+}
+
+export default App
